c0576aaab34cdf8b626dd10e56804ec430e72600
Natan Corcovia <natankorkov@gmail.com>
2025-07-25 17:53:05 -0300

    feat (java exercices) : calculating area of squares

---
+++ JAVA/Ex02.java
@@ -0,0 +1,17 @@
+// Escreva um código que receba o tamanho do lado de um quadrado, calcule sua área e exiba na tela
+// fórmula: área=lado X lado
+
+package JAVA;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Insira o tamanho do quadrado:");
+        var square = scanner.nextInt();
+        var area = square * square;
+        System.out.printf("A área do quadrado é de %d \n2" , area);
+        scanner.close();
+    }
+}
