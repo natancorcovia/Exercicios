d7159950a2d3fdf034cd336dc9c66652963038d7
Natan Corcovia <natankorkov@gmail.com>
2025-07-26 14:05:21 -0300

    feat (typescript exercise) : testing function

--- TYPESCRIPT/types/05.ts
+++ TYPESCRIPT/types/05.ts
@@ -17,3 +17,10 @@ const enviarNotificacao = (n: Notificacao) => {
   }
   console.log("Enviando SMS para...");
 };
+
+const not1: Email = { tipo: "email", endereco: "natan@email.com" };
+const not2: SMS = { tipo: "sms", numero: "15999990000" };
+
+enviarNotificacao(not1);
+
+enviarNotificacao(not2);
