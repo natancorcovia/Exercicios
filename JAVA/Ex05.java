a89e8a786bf91aab837eac42a2df30f4e61164fe
Natan Corcovia <natankorkov@gmail.com>
2025-07-29 17:13:49 -0300

    feat (java exercise) : dio challenge

---
+++ JAVA/Ex05.java
@@ -0,0 +1,26 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Ex05 {
+  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Leia o peso em quilos
+        System.out.print("Digite o peso (kg): ");
+        float peso = scanner.nextFloat();
+
+        // Leia o valor do frete por quilo
+        System.out.print("Digite o valor do frete por quilo: ");
+        float fretePorQuilo = scanner.nextFloat();
+
+        // Calcule o valor total do frete
+        float valorDoFrete = peso * fretePorQuilo;
+
+        // Exiba o valor formatado com duas casas decimais
+        System.out.printf("Valor total do frete: R$ %.2f\n", valorDoFrete);
+
+        scanner.close();
+    }
+}
---
+++ JAVA/Ex06.java
@@ -0,0 +1,25 @@
+package JAVA;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+        float valorDoProduto = scanner.nextFloat();
+        
+        System.out.println();
+        float porcentagemDeDesconto = scanner.nextFloat();
+        
+        if(porcentagemDeDesconto < 0 || porcentagemDeDesconto > 100){
+          System.out.println("Desconto invalido.");
+        } else {
+          float valorFinal = valorDoProduto - (valorDoProduto * porcentagemDeDesconto / 100);
+            DecimalFormat df = new DecimalFormat("0.00");
+            System.out.println(df.format(valorFinal));
+        }
+        scanner.close();
+    }
+}
