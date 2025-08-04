1be93d49f0942d94850cca5a1d908038cd1cf633
Natan Corcovia <natankorkov@gmail.com>
2025-08-04 01:28:20 -0300

    fix (java exercise) : eliminating duplicate value from 'Pagamento' class

--- JAVA/OOP/challenges/c03/Pagamento.java
+++ JAVA/OOP/challenges/c03/Pagamento.java
@@ -2,7 +2,6 @@ package JAVA.OOP.challenges.c03;
 
 class Pagamento extends Transacao{
     private MetodoPagamento metodoPagamento;
-    private double valor;
 
     public Pagamento (MetodoPagamento metodoPagamento , double valor){
         this.metodoPagamento = metodoPagamento;
