8dbe904afc8732b272f166813165aca5bdc23c2d
Natan Corcovia <natankorkov@gmail.com>
2025-08-14 17:22:19 -0300

    feat (java exercise): create Produto class in challenge 5

---
+++ JAVA/OOP/challenges/c05/Produto.java
@@ -0,0 +1,20 @@
+package JAVA.OOP.challenges.c05;
+
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    
+}
